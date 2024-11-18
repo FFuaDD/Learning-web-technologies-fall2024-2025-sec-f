1ce47724c4b78c470f80e21187c33d0a603627c6
FFuaDD <145370050+FFuaDD@users.noreply.github.com>
2024-11-18 16:33:04 +0600

    Add files via upload

---
+++ LAB 4/emailcheck.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if (isset($_POST['submit'])) {
+    $name = $_REQUEST['name'];
+    if (empty($name) || strpos($name, "@") == false || strpos($name, ".") == false) 
+    {
+        header('location: email.html');
+    } else {
+     
+        echo "Welcome Home";
+    }
+}
+?>

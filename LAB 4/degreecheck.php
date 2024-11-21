6147471f617107cfee0c7a196233d60c29beb34a
FFuaDD <145370050+FFuaDD@users.noreply.github.com>
2024-11-21 12:37:05 +0600

    degreecheck.php

---
+++ LAB 4/degreecheck.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if(isset($_POST['submit']))
+{
+    if (!empty($_POST['degrees']) && count($_POST['degrees']) == 2) {
+    echo "Welcome to php  " ;
+} else if(empty($_POST['degrees']) || count($_POST['degrees']) >= 2 ||count($_POST['degrees'])<= 2) {
+    echo "Please pick at least two options.";
+}
+
+    else
+    {
+        header('location:degree.html');
+    }
+}
+?>
